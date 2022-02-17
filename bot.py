ea5d269d73ba18adb4ed4c29a18bbe6c9e1ec761
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2022-02-17 12:10:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+import requests
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
@@ -44,6 +45,21 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
+
+@bot.on_message(filters.command(["logo"]))
+async def logo(_, m : Message):
+    if len(m.command) <2:
+        return await m.reply_text("Please provide a name")
+    else: 
+        try:
+            hee = await m.reply("making your logo...")
+            name = m.text.split(None, 1)[1]
+            req = requests.get(f"https://sd-logo-api.herokuapp.com/?logo={name}")
+            IMG = req.text
+            await hee.delete()
+            await m.reply_photo(IMG) 
+        except Exception as e:
+            await m.reply_text(f"Error: {e}")
 
 
 app = Bot()
